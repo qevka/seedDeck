c1e70294a52d1bf39681ed9be186565daaf276ea
Oluwatobi <tobbyzomo221@gmail.com>
2022-02-21 15:22:13 -0600

    I made changes to the DESCRIPTION of group() & test()

--- test/app/data/data_test.dart
+++ test/app/data/data_test.dart
@@ -5,10 +5,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:seed_deck/app/data/seed.dart';
 
 void main() {
-  group("app folder, data class", () {
+  group("Test data model", () {
     final seedData = File('assets/data/data.json');
-    //List<dynamic> seedJsonList;
-    test("Seed class test", () async {
+    test("SEED", () async {
       final seedJson = jsonDecode(await seedData.readAsString());
       var seed = Seed.fromJson(seedJson[7]);
       var seedResult = seed.toJson();
